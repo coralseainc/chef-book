b03b88040289765759506e966935799730d94b57
Jerry Anderson <and03755@umn.edu>
2014-01-08 11:44:34 -0600

    Adding cookbooks

---
+++ cookbooks/my_cookbook/metadata.rb
@@ -0,0 +1,13 @@
+name             'my_cookbook'
+maintainer       'Coral Sea Inc.'
+maintainer_email 'chef@coralseainc.com'
+license          'All rights reserved'
+description      'Installs/Configures my_cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'build-essential'
+depends 'apache2', '>= 1.0.4'
+depends 'chef-client'
+depends 'yum'
+depends 'ntp'
---
+++ cookbooks/my_cookbook/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: my_cookbook
+# Recipe:: default
+#
+# Copyright 2013, Coral Sea Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "chef-client"
+include_recipe "yum"
+include_recipe "ntp"
