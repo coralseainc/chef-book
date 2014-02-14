7c10ab37f414305d5b73586af8eef7bdc2a66d79
Jerry Anderson <and03755@umn.edu>
2014-02-14 10:12:55 -0600

    Metadata

--- cookbooks/my_cookbook/metadata.rb
+++ cookbooks/my_cookbook/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chef@coralseainc.com'
 license          'All rights reserved'
 description      'Installs/Configures my_cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'build-essential'
 depends 'apache2', '>= 1.0.4'
